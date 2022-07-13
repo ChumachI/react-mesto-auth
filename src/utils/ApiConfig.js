291ba31e573f7156e49ab18e561c74b24f9ab466
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-07-13 12:46:41 +0300

    hotfix/использовать ошибки авторизации-1

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -2,8 +2,7 @@
 const ApiConfig = {
     commonUrlPart: 'http://api.mesto.ilya.chumak.nomoredomains.xyz',
     headers: {
-        authorization: '074637ff-87eb-4ca4-82ca-28b979d992aa',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       }
 }
 export { ApiConfig }
